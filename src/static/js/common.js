2aead295e77cd37108be317337728f6ae47001fb
Aslan <berezg86@gmail.com>
2020-08-30 14:22:27 +0300

    Шапка под мобилу, выпадающее меню

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
 
+    $('.burger').click(function () {
+        $(this).toggleClass('is-active');
+        $('.dropdown-menu').toggleClass('is-active');
+    });
+
+    $(".mob-nav>li").click(function () {
+        var $this = $(this);
+        var elem = $(this).find('.mob-nav__inner');
+        if (!$this.hasClass("is-active")) {
+            $(".mob-nav__inner").slideUp();
+            $(".mob-nav>li").removeClass("is-active");
+        }
+        $this.toggleClass("is-active");
+        elem.slideToggle();
+    });
+
 });
