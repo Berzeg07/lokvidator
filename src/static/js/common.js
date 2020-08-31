c02b3c5975d497c3b34b937f1c931f516f690653
Aslan <berezg86@gmail.com>
2020-08-31 15:43:59 +0300

    Преимущества

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -64,6 +64,46 @@ $(document).ready(function () {
 
     initAction();
 
+    var advSliser = undefined;
+
+    function initAdvSlider() {
+        var screenWidth = $(window).width();
+        if (screenWidth < 1280 && advSliser == undefined) {
+            var advSliser = new Swiper('.adv-slider', {
+                slidesPerView: '1',
+                spaceBetween: 35,
+                loop: true,
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                breakpoints: {
+                    767: {
+                        slidesPerView: 1,
+                        spaceBetween: 30,
+                    },
+                    991: {
+                        slidesPerView: 2,
+                        spaceBetween: 30,
+                    },
+                    1279: {
+                        slidesPerView: 3,
+                        spaceBetween: 40,
+                    }
+                }
+            });
+        } else if (screenWidth > 1279 && advSliser != undefined) {
+            advSliser.destroy();
+            advSliser = undefined;
+        }
+    }
+
+    initAdvSlider();
+
     $(window).on('resize', function () {
         initAction();
     });
