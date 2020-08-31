7134f6970877d3ea174c53be1b642a10d4ee6fea
Aslan <berezg86@gmail.com>
2020-08-31 12:27:04 +0300

    Об услугах

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -30,4 +30,42 @@ $(document).ready(function () {
         },
     });
 
+    var aboutSliser = undefined;
+
+    function initAction() {
+        var screenWidth = $(window).width();
+        if (screenWidth > 991 && aboutSliser == undefined) {
+            var aboutSliser = new Swiper('.about-slider', {
+                slidesPerView: '3',
+                spaceBetween: 35,
+                loop: true,
+                navigation: {
+                    nextEl: '.swiper-button-next-as',
+                    prevEl: '.swiper-button-prev-as',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                breakpoints: {
+                    1599: {
+                        slidesPerView: 2,
+                        spaceBetween: 30,
+                    }
+                }
+            });
+        } else if (screenWidth < 992 && aboutSliser != undefined) {
+            aboutSliser.destroy();
+            aboutSliser = undefined;
+            // $('.scrollbar-slider__wrapper').removeAttr('style');
+            // $('.scrollbar-slider__slide').removeAttr('style');
+        }
+    }
+
+    initAction();
+
+    $(window).on('resize', function () {
+        initAction();
+    });
+
 });
